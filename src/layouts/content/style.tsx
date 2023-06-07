aa2dfcd9769f62428ae81d57990f516c408cf491
lamlqb <lamlqb@bap.jp>
2023-06-07 15:01:40 +0700

    fix content

--- src/layouts/content/style.tsx
+++ src/layouts/content/style.tsx
@@ -2,9 +2,7 @@ import styled from 'styled-components';
 import Container from '@mui/material/Container';
 
 export const ContentLayoutStyle = styled(Container)`
-  &.css-1oqqzyl-MuiContainer-root {
-    max-width: 100%;
-    background-color: antiquewhite;
-    min-height: 500px;
-  }
+  max-width: 100%;
+  background-color: antiquewhite;
+  min-height: 500px;
 `;
