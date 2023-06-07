ab4506386908208f63e9a87bb0698b1f6d9ef72f
lamlqb <lamlqb@bap.jp>
2023-06-07 16:02:02 +0700

    fix deploy

--- pages/services/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import { ServicesPageStyle } from './style';
-
-const ServicesPage: React.FC = () => {
-  return <ServicesPageStyle>Services Page</ServicesPageStyle>;
-};
-
-export default ServicesPage;
--- pages/services/style.tsx
+++
@@ -1,4 +0,0 @@
-import styled from 'styled-components';
-import Grid from '@mui/material/Grid';
-
-export const ServicesPageStyle = styled(Grid)``;
