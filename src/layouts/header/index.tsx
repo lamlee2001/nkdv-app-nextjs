28da9d06bce4e9d8c6eb3dc163173484cb373d3d
lamlqb <lamlqb@bap.jp>
2023-06-07 14:56:14 +0700

    fix bugs

--- src/layouts/header/index.tsx
+++ src/layouts/header/index.tsx
@@ -116,7 +116,7 @@ const HeaderLayout: React.FC = () => {
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                <Avatar alt="Remy Sharp" src="https://tapchilamdep.com/wp-content/uploads/2019/10/Rose-887x1024.jpg" />
               </IconButton>
             </Tooltip>
             <Menu
