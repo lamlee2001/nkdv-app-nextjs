485c5d02eccfea65005b4ee5c1395b897d57d920
lamlqb <lamlqb@bap.jp>
2023-06-13 15:42:41 +0700

    create price

--- src/pages/pricing/index.tsx
+++ src/pages/pricing/index.tsx
@@ -1,9 +1,119 @@
 import React from 'react';
+import { Avatar, Divider, Grid, Typography } from '@mui/material';
+import Breadcum from '@/src/components/breadcum';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
+import MESS_IMG from 'src/assets/image/messenger_image.png';
+import ZALO_IMG from 'src/assets/image/zalo_image.png';
+import PRICE_1 from 'src/assets/image/price_1.jpg';
+import PRICE_2 from 'src/assets/image/price_2.jpg';
+import PRICE_3 from 'src/assets/image/price_3.jpg';
+import PRICE_4 from 'src/assets/image/price_4.jpg';
+import PRICE_5 from 'src/assets/image/price_5.jpg';
+import PRICE_6 from 'src/assets/image/price_6.jpg';
+import PRICE_7 from 'src/assets/image/price_7.jpg';
+import PRICE_9 from 'src/assets/image/price_9.jpg';
+import PRICE_10 from 'src/assets/image/price_10.jpg';
+import PRICE_11 from 'src/assets/image/price_11.png';
+import PRICE_12 from 'src/assets/image/price_12.jpg';
+import PRICE_13 from 'src/assets/image/price_13.jpg';
+import PRICE_14 from 'src/assets/image/price_14.jpg';
+import PRICE_15 from 'src/assets/image/price_15.jpg';
 import { PricingPageStyle } from './style';
 
 const PricingPage: React.FC = () => {
-  return <PricingPageStyle>Pricing Page</PricingPageStyle>;
+  const rows = [
+    { image: PRICE_1, type: 'Răng hàm giả tháo lắp', price: '220.000 Đ - 720.000 Đ' },
+    { image: PRICE_2, type: 'Trám răng thẩm mỹ', price: '150.000 Đ - 220.000 Đ' },
+    { image: PRICE_3, type: 'Điều trị Tủy', price: '400.000 Đ - 720.000 Đ' },
+    { image: PRICE_4, type: 'Điều trị viên nha chu', price: '250.000 Đ - 500.000 Đ' },
+    { image: PRICE_5, type: 'Tẩy trắng răng bằng đèn LUMA COOL 1 giờ', price: '1.600.000 Đ - 2.350.000 Đ' },
+    { image: PRICE_6, type: 'Điều trị cười hở lợi', price: '4.000.000 Đ - 6.000.000 Đ' },
+    { image: PRICE_7, type: 'Nhổ răng khôn số 8', price: '720.000 Đ - 2.000.000 Đ' },
+    { image: PRICE_9, type: 'Niềng răng - Chỉnh Nha', price: '5.000.000 Đ - 30.000.000 Đ' },
+    { image: PRICE_9, type: 'Răng sứ Mỹ', price: '1.000.000 Đ' },
+    { image: PRICE_10, type: 'Răng sứ Titan', price: '2.200.000 Đ' },
+    { image: PRICE_11, type: 'Răng sứ Ziconia', price: '4.300.000 Đ' },
+    { image: PRICE_12, type: 'Răng sứ Cercon', price: '4.900.000 Đ' },
+    { image: PRICE_13, type: 'Răng sứ Emax', price: '6.500.000 Đ' },
+    { image: PRICE_14, type: 'Răng sứ Venner', price: '8.000.000 Đ' },
+    { image: PRICE_15, type: 'Cấy ghép Implant', price: '15.000.000 Đ - 30.000.000 Đ' },
+  ];
+
+  return (
+    <PricingPageStyle container>
+      <Grid item xs={12} className="banner-pricing">
+        <Typography className="title fzm-16">Bảng giá</Typography>
+      </Grid>
+
+      <Grid container className="content">
+        <Grid item xs={12} md={9} className="grid-left">
+          <Breadcum>Bảng giá</Breadcum>
+
+          <Typography className="title-content">Bảng giá các dịch vụ tại nha khoa thẩm mỹ Đại Việt</Typography>
+
+          <Divider className="divider" />
+
+          <Grid container>
+            <Grid item xs={12} className="grid-image" />
+            <Typography className="text-1">Tại Sao Bạn Chọn Chúng Tôi – Nha Khoa Đại Việt Đà Nẵng?</Typography>
+            <Typography className="text-2">
+              Người ta vẫn thường nói: “muốn có tầm, thì phải có tâm”, do đó Nha Khoa Đại Việt không bao giờ dùng những
+              câu từ có cánh hay những câu nói mỹ miều..
+            </Typography>
+            <Typography>
+              Tân trang cho nụ cười không đơn giản là “mặc một chiếc áo mới” cho hàm răng như nhiều khách hàng vẫn nghĩ.
+              Bởi nó là cả một quá trình mang tâm huyết, tấm lòng và kinh nghiệm của một người bác sĩ vào từng chiếc
+              răng. Bởi chúng tôi luôn quan niệm rằng một cuộc điều trị thành công không chỉ cho một hàm răng thẩm mỹ mà
+              còn là sức khỏe và sự an toàn của khách hàng.
+            </Typography>
+          </Grid>
+
+          <TableContainer component={Paper} className="table">
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell align="left">Image</TableCell>
+                  <TableCell align="left">Type</TableCell>
+                  <TableCell align="left">Price</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {rows.map((item, index) => (
+                  <TableRow key={index} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                    <TableCell align="left">
+                      <Avatar variant="square" src={item.image.src}/>
+                    </TableCell>
+                    <TableCell align="left">{item.type}</TableCell>
+                    <TableCell align="left">{item.price}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Grid>
+
+        <Grid item xs={0} md={3} className="grid-right">
+          <Typography className="title-right">Hỗ trợ trực tuyến</Typography>
+
+          <Grid container className="container-right">
+            <Typography>Hotline:</Typography>
+            <Typography className="phone">0905 644 659</Typography>
+            <Grid item xs={12} className="grid-icon">
+              <Avatar src={MESS_IMG.src} className="icon" />
+              <Avatar src={ZALO_IMG.src} className="icon" />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </PricingPageStyle>
+  );
 };
 
 export default PricingPage;
--- src/pages/pricing/style.tsx
+++ src/pages/pricing/style.tsx
@@ -1,4 +1,321 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import Grid from '@mui/material/Grid';
 
-export const PricingPageStyle = styled(Grid)``;
+import ResponsiveUI, { SIZE, SIZEH } from '@/src/constants/responsive';
+
+import IMAGE_PRICE from 'src/assets/image/pricing_image.jpg';
+import PRICING_IMAGE from 'src/assets/image/image_pricing.png';
+
+const pricingResponsive = css`
+  ${ResponsiveUI.lessThanH(SIZEH.PAD3)} {
+    content: 'PAD3';
+    ${ResponsiveUI.lessThan(SIZE.XXL2)} {
+      content: 'PAD3 - XXL2';
+    }
+    ${ResponsiveUI.lessThan(SIZE.XXL)} {
+      content: 'PAD3 - XXL';
+    }
+    ${ResponsiveUI.lessThan(SIZE.XL2)} {
+      .content {
+        padding: 20px 150px;
+      }
+    }
+    ${ResponsiveUI.lessThan(SIZE.XL1)} {
+      content: 'PAD3 - XL1';
+    }
+    ${ResponsiveUI.lessThan(SIZE.XL)} {
+      .content {
+        padding: 20px 100px;
+      }
+    }
+    ${ResponsiveUI.lessThan(SIZE.LG)} {
+      content: 'PAD3 - LG';
+    }
+    ${ResponsiveUI.lessThan(SIZE.MD)} {
+      .content {
+        padding: 20px;
+      }
+    }
+    ${ResponsiveUI.lessThan(SIZE.SM)} {
+      content: 'PAD3 - SM';
+    }
+    ${ResponsiveUI.lessThan(SIZE.XS)} {
+      content: 'PAD3 - XS';
+    }
+  }
+  ${ResponsiveUI.lessThanH(SIZEH.PAD2)} {
+    content: 'PAD2';
+    ${ResponsiveUI.lessThan(SIZE.XXL2)} {
+    }
+    ${ResponsiveUI.lessThan(SIZE.XXL)} {
+      content: 'PAD2 - XXL';
+    }
+    ${ResponsiveUI.lessThan(SIZE.XL2)} {
+    }
+    ${ResponsiveUI.lessThan(SIZE.XL1)} {
+      content: 'PAD2 - XL1';
+    }
+    ${ResponsiveUI.lessThan(SIZE.XL)} {
+    }
+    ${ResponsiveUI.lessThan(SIZE.LG)} {
+      content: 'PAD2 - LG';
+    }
+    ${ResponsiveUI.lessThan(SIZE.MD)} {
+    }
+    ${ResponsiveUI.lessThan(SIZE.SM)} {
+      content: 'PAD2 - SM';
+    }
+    ${ResponsiveUI.lessThan(SIZE.XS)} {
+      content: 'PAD2 - XS';
+    }
+  }
+  ${ResponsiveUI.lessThanH(SIZEH.PAD1)} {
+    content: 'PAD1';
+    ${ResponsiveUI.lessThan(SIZE.XXL2)} {
+      content: 'PAD1 - XXL2';
+    }
+    ${ResponsiveUI.lessThan(SIZE.XXL)} {
+      content: 'PAD1 - XXL';
+    }
+    ${ResponsiveUI.lessThan(SIZE.XL2)} {
+      content: 'PAD1 - XL2';
+    }
+    ${ResponsiveUI.lessThan(SIZE.XL1)} {
+      content: 'PAD1 - XL1';
+    }
+    ${ResponsiveUI.lessThan(SIZE.XL)} {
+      content: 'PAD1 - XL';
+    }
+    ${ResponsiveUI.lessThan(SIZE.LG)} {
+      content: 'PAD1 - LG';
+    }
+    ${ResponsiveUI.lessThan(SIZE.MD)} {
+      content: 'PAD1 - MD';
+    }
+    ${ResponsiveUI.lessThan(SIZE.SM)} {
+      .banner-pricing {
+        height: 200px;
+      }
+
+      .content {
+        padding: 20px;
+
+        .grid-left {
+          .grid-image {
+            height: 150px;
+          }
+        }
+
+        .grid-right {
+          display: none;
+        }
+      }
+    }
+    ${ResponsiveUI.lessThan(SIZE.XS)} {
+      content: 'PAD1 - XS';
+    }
+  }
+  ${ResponsiveUI.lessThanH(SIZEH.MAC16)} {
+    content: 'MAC16';
+    ${ResponsiveUI.lessThan(SIZE.XXL2)} {
+      content: 'MAC16 - XXL2';
+    }
+    ${ResponsiveUI.lessThan(SIZE.XXL)} {
+      content: 'MAC16 - XXL';
+    }
+    ${ResponsiveUI.lessThan(SIZE.XL2)} {
+      content: 'MAC16 - XL2';
+    }
+    ${ResponsiveUI.lessThan(SIZE.XL1)} {
+      content: 'MAC16 - XL1';
+    }
+    ${ResponsiveUI.lessThan(SIZE.XL)} {
+      content: 'MAC16 - XL';
+    }
+    ${ResponsiveUI.lessThan(SIZE.LG)} {
+      content: 'MAC16 - LG';
+    }
+    ${ResponsiveUI.lessThan(SIZE.MD)} {
+      content: 'MAC16 - MD';
+    }
+    ${ResponsiveUI.lessThan(SIZE.SM)} {
+      content: 'MAC16 - SM';
+    }
+    ${ResponsiveUI.lessThan(SIZE.XS)} {
+      content: 'MAC16 - XS';
+    }
+  }
+  ${ResponsiveUI.lessThanH(SIZEH.MAC14)} {
+    content: 'MAC14';
+    ${ResponsiveUI.lessThan(SIZE.XXL2)} {
+      content: 'MAC14 - XXL2';
+    }
+    ${ResponsiveUI.lessThan(SIZE.XXL)} {
+      content: 'MAC14 - XXL';
+    }
+    ${ResponsiveUI.lessThan(SIZE.XL2)} {
+      content: 'MAC14 - XL2';
+    }
+    ${ResponsiveUI.lessThan(SIZE.XL1)} {
+      content: 'MAC14 - XL1';
+    }
+    ${ResponsiveUI.lessThan(SIZE.XL)} {
+      content: 'MAC14 - XL';
+    }
+    ${ResponsiveUI.lessThan(SIZE.LG)} {
+      content: 'MAC14 - LG';
+    }
+    ${ResponsiveUI.lessThan(SIZE.MD)} {
+      content: 'MAC14 - MD';
+    }
+    ${ResponsiveUI.lessThan(SIZE.SM)} {
+      content: 'MAC14 - SM';
+    }
+    ${ResponsiveUI.lessThan(SIZE.XS)} {
+      content: 'MAC14 - XS';
+    }
+  }
+  ${ResponsiveUI.lessThanH(SIZEH.MAC13)} {
+    content: 'MAC13';
+    ${ResponsiveUI.lessThan(SIZE.XXL2)} {
+      content: 'MAC13 - XXL2';
+    }
+    ${ResponsiveUI.lessThan(SIZE.XXL)} {
+      content: 'MAC13 - XXL';
+    }
+    ${ResponsiveUI.lessThan(SIZE.XL2)} {
+      content: 'MAC13 - XL2';
+    }
+    ${ResponsiveUI.lessThan(SIZE.XL1)} {
+      content: 'MAC13 - XL1';
+    }
+    ${ResponsiveUI.lessThan(SIZE.XL)} {
+      content: 'MAC13 - XL';
+    }
+    ${ResponsiveUI.lessThan(SIZE.LG)} {
+      content: 'MAC13 - LG';
+    }
+    ${ResponsiveUI.lessThan(SIZE.MD)} {
+      content: 'MAC13 - MD';
+    }
+    ${ResponsiveUI.lessThan(SIZE.SM)} {
+      content: 'MAC13 - SM';
+    }
+    ${ResponsiveUI.lessThan(SIZE.XS)} {
+      content: 'MAC13 - XS';
+    }
+  }
+`;
+
+export const PricingPageStyle = styled(Grid)`
+  .banner-pricing {
+    height: 500px;
+    background-image: url(${PRICING_IMAGE.src});
+    background-repeat: no-repeat;
+    background-size: cover;
+    position: relative;
+
+    .title {
+      position: absolute;
+      color: #ffffff;
+      align-items: center;
+      justify-content: center;
+      height: 100%;
+      width: 100%;
+      display: flex;
+      font-size: 32px;
+      font-weight: 700;
+      text-transform: uppercase;
+      text-shadow: 1px 1px 3px rgb(0 0 0 / 50%);
+    }
+
+    &::before {
+      content: '';
+      position: absolute;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      margin: auto;
+      background-color: rgb(41 41 41 / 59%);
+    }
+  }
+
+  .content {
+    padding: 20px 300px;
+
+    .grid-left {
+      padding-right: 20px;
+
+      .grid-image {
+        background-image: url(${IMAGE_PRICE.src});
+        height: 300px;
+        background-size: 100% 100%;
+      }
+
+      .text-1 {
+        margin-top: 20px;
+        margin-bottom: 10px;
+        font-weight: 600;
+      }
+
+      .text-2 {
+        margin-bottom: 20px;
+      }
+
+      .title-content {
+        margin-top: 20px;
+        font-size: 24px;
+        font-weight: 500;
+      }
+
+      .divider {
+        border-width: 1px;
+        border-color: #1976d2;
+        margin-bottom: 20px;
+      }
+
+      .table {
+        margin-top: 10px;
+      }
+    }
+
+    .grid-right {
+      box-shadow: 0 0 3px 1px rgb(0 0 0 / 25%);
+      height: fit-content;
+
+      .title-right {
+        color: red;
+        text-align: center;
+        text-transform: uppercase;
+        margin-top: 10px;
+        padding-bottom: 5px;
+        border-bottom: 2px solid #ff0000;
+      }
+
+      .container-right {
+        padding: 10px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        .phone {
+          font-size: 22px;
+          font-weight: 600;
+          margin-left: 5px;
+        }
+
+        .grid-icon {
+          display: flex;
+          justify-content: center;
+
+          .icon {
+            margin-right: 10px;
+          }
+        }
+      }
+    }
+  }
+
+  ${pricingResponsive}
+`;
